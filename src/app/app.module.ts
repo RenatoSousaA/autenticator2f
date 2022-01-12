db306096a9015db4cd4d26671eeac43f4095acfd
Renato Sousa <renato.sousainfo@gmail.com>
2022-01-12 15:57:52 -0300

    Iniciando estudo de como conectar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MsalModule, MSAL_INSTANCE, MsalService } from '@azure/msal-angular'
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '0f7ca74b-0823-4e59-b875-04367fafd408',
+      redirectUri: 'https://autentication-app-2f.herokuapp.com'
+    }
+  })
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +21,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
